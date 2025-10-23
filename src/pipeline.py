d2ae9c13d2817a98c4f0c72b51aacbebf1ad00c9
Taqi Hamoda <taqihisham@gmail.com>
2025-10-23 17:33:06 +0200

    removed uiqm

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2, csv, gc
 from typing import List, Tuple, Dict
-from time import perf_counter
 
 from .colormap import Colormap
 from .cameras import Sensor
@@ -23,7 +22,7 @@ class Pipeline:
         "area (pixels)",
         "ground res. (mm / pixel)",
         "camera slant (degrees)",
-        "UIQM",
+        # "UIQM",
         "UCIQUE",
     ]
 
@@ -56,7 +55,7 @@ class Pipeline:
             mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
 
             slant = calculate_slant(self.sensor, depth)
-            uiqm = calculate_UIQM(img)
+            # uiqm = calculate_UIQM(img)
             ucique = calculate_UCIQE(img)
 
             for color in np.unique(mask.reshape(-1, mask.shape[-1]), axis=0):
@@ -82,7 +81,7 @@ class Pipeline:
                     "area (pixels)": u.size,
                     "ground res. (mm / pixel)": calculate_ground_resolution(self.sensor, u, v, depth[u, v]),
                     "camera slant (degrees)": slant,
-                    "UIQM": uiqm,
+                    # "UIQM": uiqm,
                     "UCIQUE": ucique
                 })
 
